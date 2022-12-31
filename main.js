5cfa1b483181d8de98964f846ee933027127fa3f
ozeiasmello <ozeiasmello@live.com>
2022-12-31 07:39:49 -0300

    usando o parseInt

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 document.getElementById("formulario").addEventListener("submit", function(event) {
     event.preventDefault(); // impede o envio do formulário
-  
+
     // obtém os valores dos campos A e B
-    var campoA = document.getElementById("campoA").value;
-    var campoB = document.getElementById("campoB").value;
-  
-    // verifica se o valor do campo B é maior que o valor do campo A
+    let campoA = document.getElementById("campoA").value;
+    let campoB = document.getElementById("campoB").value;
+    let campo = parseInt(campoA, campoB);
+
+
+// verifica se o valor do campo B é maior que o valor do campo A
     if (campoB > campoA) {
-      alert("Formulário válido!");
+        alert("Formulário válido!");
     } else {
-      alert("Formulário inválido!");
-    }
-  });
-  
+        (campoB < campoA) 
+            alert("Formulário inválido!");
+}});
+
