156eee1c4f549265f80c282ac9bb4a02686ea4ba
ozeiasmello <ozeiasmello@live.com>
2023-01-05 14:06:19 -0300

    Colocando o parseInt

--- main.js
+++ main.js
@@ -2,16 +2,20 @@ document.getElementById("formulario").addEventListener("submit", function(event)
     event.preventDefault(); // impede o envio do formulário
 
     // obtém os valores dos campos A e B
-    let campoA = document.getElementById("campoA").value;
-    let campoB = document.getElementById("campoB").value;
-    let campo = parseInt(campoA, campoB);
+    let campoA = parseInt(document.getElementById("campoA").value);
+    let campoB = parseInt(document.getElementById("campoB").value);
+    
+
+    let campo = parseInt(campoA);
+    let campo2 = parseInt(campoB);
+
 
 
 // verifica se o valor do campo B é maior que o valor do campo A
-    if (campoB > campoA) {
+    if (campo2 > campo) {
         alert("Formulário válido!");
     } else {
-        (campoB < campoA) 
+        (campo2 < campo) 
             alert("Formulário inválido!");
 }});
 
