1198133cacb757dadf7dd310031d35d7145ef904
ozeiasmello <ozeiasmello@live.com>
2023-01-08 09:18:27 -0300

    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+
+    $('#telefone').mask('(00) 00000-0000')
+
+    $('#cpf-titular').mask('000.000.000-00')
+
+    $('#cep-titular').mask('00000-000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true,
+            },
+            
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome'
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos){
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+
+        $('html').animate({
+            scrollTop: destino.offset().top
+        }, 1000)
