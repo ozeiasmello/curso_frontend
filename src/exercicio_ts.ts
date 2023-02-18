29f7607ecb7652dfdcbd492d6c289c4178c66b5c
ozeiasmello <ozeiasmello@live.com>
2023-02-18 11:08:03 -0300

    Primeiro commit.

---
+++ src/exercicio_ts.ts
@@ -0,0 +1,9 @@
+// Função de multiplicação
+function multiplicar(x: number, y: number): number {
+  return x * y;
+}
+
+// Função de saudação
+function saudar(nome: string): string {
+    return `Olá ${nome}`;
+}
